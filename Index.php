a40fcd8f52527c501f4f74c1d4bdd7f96348e347
Champaloux Benjamin <tiitanyum@gmail.com>
2018-02-21 11:01:30 +0100

    start

---
+++ Index.php
@@ -0,0 +1,364 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="no-js oldie ie8" lang="fr"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js oldie ie9" lang="fr"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="fr"> <!--<![endif]-->
+    <head>
+
+        <!--- basic page needs
+================================================== -->
+        <meta charset="utf-8">
+        <title>Champaloux Benjamin</title>
+
+        <!-- mobile specific metas
+================================================== -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <!-- CSS
+================================================== -->
+        <link rel="stylesheet" href="assets/css/base.css">
+        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/css/vendor.css">
+
+        <!-- script
+================================================== -->
+        <script src="js/modernizr.js"></script>
+        <script src="js/pace.min.js"></script>
+
+        <!-- favicons
+================================================== -->
+    </head>
+
+    <body id="top">
+
+        <!-- intro section
+================================================== -->
+        <section id="intro">
+
+            <div class="intro-overlay"></div>
+
+            <div class="intro-content">
+                <div class="row">
+
+                    <div class="col-twelve">
+
+                        <h5></h5>
+                        <h1>Nathan Lepers</h1>
+
+                        <p class="intro-position">
+                            <span>Apprenant Developpeur Web</span>
+                        </p>
+
+                        <a class="button stroke smoothscroll" href="#about" title="">En Savoir Plus</a>
+
+                    </div>
+
+                </div>
+            </div> <!-- /intro-content -->
+
+            <ul class="intro-social">
+                <li><a href="#"><i class="fa fa-github"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            </ul> <!-- /intro-social -->
+
+        </section> <!-- /intro -->
+
+        <section id="resume" class="grey-section">
+
+            <div class="row section-intro">
+                <div class="col-twelve">
+                    <h1>Parcours</h1>
+                </div>
+            </div> <!-- /section-intro-->
+
+            <div class="row resume-timeline">
+
+                <div class="col-twelve resume-header">
+
+                    <h2>Formations</h2>
+
+                </div> <!-- /resume-header -->
+
+                <div class="col-twelve">
+
+                    <div class="timeline-wrap">
+
+                        <div class="timeline-block">
+
+                            <div class="timeline-ico">
+                                <i class="fa fa-graduation-cap"></i>
+                            </div>
+
+                            <div class="timeline-header">
+                                <h3>Formation Developpeur</h3>
+                                <p>Septembre 2016 - Présent</p>
+                            </div>
+
+                            <div class="timeline-content">
+                                <h4>UpTo</h4>
+                                <p>Formation labellisée Grande École du Numérique.en developpement Web; Front-End, Back-End. Intensif, 90% pratique. Learning by doing, Learning by teaching.</p>
+                            </div>
+
+                        </div> <!-- /timeline-block -->
+
+                        <div class="timeline-block">
+
+                            <div class="timeline-ico">
+                                <i class="fa fa-graduation-cap"></i>
+                            </div>
+
+                            <div class="timeline-header">
+                                <h3>Étudiant</h3>
+                                <p>Septembre 2014 - Juin 2016</p>
+                            </div>
+
+                            <div class="timeline-content">
+                                <h4>Universités</h4>
+                                <p>1ère année d'IUT informatique UM2 suivie d'une <br>1ère année en fac informatique UM2 </p>
+                            </div>
+
+                        </div> <!-- /timeline-block -->
+
+                    </div> <!-- /timeline-wrap -->
+
+                </div> <!-- /col-twelve -->
+
+            </div> <!-- /resume-timeline -->
+
+            <div class="row resume-timeline">
+
+                <div class="col-twelve resume-header">
+
+                    <h2>Évenements</h2>
+
+                </div> <!-- /resume-header -->
+
+                <div class="col-twelve">
+
+                    <div class="timeline-wrap">
+
+                        <div class="timeline-block">
+
+                            <div class="timeline-ico">
+                                <i class="fa fa-briefcase"></i>
+                            </div>
+
+                            <div class="timeline-header">
+                                <h3>Hackathon Toulouse</h3>
+                                <p>Novembre 2016</p>
+                            </div>
+
+                            <div class="timeline-content">
+                                <h4>Futurapolis</h4>
+                                <p>Hackathon FuturaPolis Toulouse sur la thématique des voitures autonomes.</p>
+                            </div>
+
+                        </div> <!-- /timeline-block -->
+
+                    </div> <!-- /timeline-wrap -->
+
+                </div> <!-- /col-twelve -->
+
+            </div> <!-- /resume-timeline -->
+
+        </section> <!-- /features -->
+
+
+        <!-- Portfolio Section
+================================================== -->
+        <section id="portfolio">
+
+            <div class="row section-intro">
+                <div class="col-twelve">
+                    <h5>Portfolio</h5>
+                    <h1>Projets Réalisés</h1>
+                </div>
+            </div> <!-- /section-intro-->
+
+            <div class="row portfolio-content">
+
+                <div class="col-twelve">
+
+                    <!-- portfolio-wrapper -->
+                    <div id="folio-wrapper" class="block-1-2 block-mob-full stack">
+
+                        <div class="bgrid folio-item">
+                            <div class="item-wrap">
+                                <img src="images/portfolio/stroch.png" alt="Liberty">
+                                <a href="#modal-01" class="overlay">
+                                    <div class="folio-item-table">
+                                        <div class="folio-item-cell">
+                                            <h3 class="folio-title">StRoch</h3>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div> <!-- /folio-item -->
+
+                        <div class="bgrid folio-item">
+                            <div class="item-wrap">
+                                <img src="images/portfolio/yayaoke.png" alt="Ya-YaOke">
+                                <a href="#modal-02" class="overlay">
+                                    <div class="folio-item-table">
+                                        <div class="folio-item-cell">
+                                            <h3 class="folio-title">Ya-YaOke</h3>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div> <!-- /folio-item -->
+
+                        <div class="bgrid folio-item">
+                            <div class="item-wrap">
+                                <img src="images/portfolio/IntegrationModal.png" alt="Integration Modal">
+                                <a href="#modal-03" class="overlay">
+                                    <div class="folio-item-table">
+                                        <div class="folio-item-cell">
+                                            <h3 class="folio-title">Integration Modal</h3>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div> <!-- /folio-item -->
+
+                        <div class="bgrid folio-item">
+                            <div class="item-wrap">
+                                <img src="images/portfolio/Integration4.png" alt="Integration">
+                                <a href="#modal-04" class="overlay">
+                                    <div class="folio-item-table">
+                                        <div class="folio-item-cell">
+                                            <h3 class="folio-title">Integration</h3>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div> <!-- /folio-item -->
+                        <!-- modal popups - begin
+============================================================= -->
+                        <div id="modal-01" class="popup-modal slider mfp-hide">
+
+                            <div class="media">
+                                <img src="images/portfolio/stroch.png" alt="StRoch" />
+                            </div>
+
+                            <div class="description-box">
+                                <h4>StRoch</h4>
+                                <p>Alpha de site vitrine pour un hotel</p>
+
+                                <div class="categories">Web Development</div>
+                            </div>
+
+                            <div class="link-box">
+                                <a href="https://ksogaki.github.io/StRoch/">Details</a>
+                                <a href="#" class="popup-modal-dismiss">Fermer</a>
+                            </div>
+
+                        </div> <!-- /modal-01 -->
+
+                        <div id="modal-02" class="popup-modal slider mfp-hide">
+
+                            <div class="media">
+                                <img src="images/portfolio/yayaoke.png" alt="Ya-YaOke" />
+                            </div>
+
+                            <div class="description-box">
+                                <h4>Ya-YaOke</h4>
+                                <p>Ya-YaOke est un site de karaoke 'yaourt' en ligne (actuellement en construction)</p>
+
+                                <div class="categories">Web Design</div>
+                            </div>
+
+                            <div class="link-box">
+                                <a></a>
+                                <a href="#" class="popup-modal-dismiss">Fermer</a>
+                            </div>
+
+                        </div> <!-- /modal-02 -->
+
+                        <div id="modal-03" class="popup-modal slider mfp-hide">
+
+                            <div class="media">
+                                <img src="images/portfolio/IntegrationModal.png" alt="Integration Modal" />
+                            </div>
+
+                            <div class="description-box">
+                                <h4>Integration Modal</h4>
+                                <p>Integration pour s'entrainer sur l'ouverture et la fermeture d'un modal en jQuerry</p>
+
+                                <div class="categories">Web Design</div>
+                            </div>
+
+                            <div class="link-box">
+                                <a href="https://ksogaki.github.io/IntegrationModal/">Details</a>
+                                <a href="#" class="popup-modal-dismiss">Fermer</a>
+                            </div>
+
+                        </div> <!-- /modal-03 -->
+
+                        <div id="modal-04" class="popup-modal slider mfp-hide">
+
+                            <div class="media">
+                                <img src="images/portfolio/Integration4.png" alt="<Integration></Integration>" />
+                            </div>
+
+                            <div class="description-box">
+                                <h4>Integration</h4>
+                                <p>Integration Html/Css</p>
+
+                                <div class="categories">Web Design</div>
+                            </div>
+
+                            <div class="link-box">
+                                <a href="https://ksogaki.github.io/Integration4/">Details</a>
+                                <a href="#" class="popup-modal-dismiss">Fermer</a>
+                            </div>
+
+                        </div> <!-- /modal-04 -->
+
+                        <!-- modal popups - end
+============================================================= -->
+
+                    </div> <!-- /portfolio-wrapper -->
+
+                </div>  <!-- /twelve -->
+
+            </div> <!-- /portfolio-content -->
+
+        </section> <!-- /portfolio -->
+
+        <!-- services Section
+================================================== -->
+        <section id="services">
+
+            <div class="row section-intro">
+                <div class="col-twelve">
+                    <h5>Compétences</h5>
+                    <h1>Langages Utilisés</h1>
+                </div>
+            </div> <!-- /section-intro -->
+
+            <div class="flexskill">
+
+                <img src="images/skill/html5.png" alt="">
+                <img src="images/skill/css3.png" alt="">
+                <img src="images/skill/js.png" alt="">
+                <img src="images/skill/angularjs.png" alt="">
+                <img id="wpress" src="images/skill/wordpress.png" alt="">
+                <img src="images/skill/git.png" alt="">
+
+            </div> <!-- /services-content -->
+
+        </section> <!-- /services -->
+
+        <div id="preloader">
+            <div id="loader"></div>
+        </div>
+
+        <!-- Java Script
+================================================== -->
+        <script src="js/jquery-2.1.3.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+    </body>
+
+</html>
